4348a528eda6d9e4d89d020fb9cacb9f0d8f0096
zyp <759148450@qq.com>
2019-06-16 11:43:04 +0800

    客房管理（功能有待优化）

---
+++ src/main/java/com/neuedu/pojo/Rooms.java
@@ -0,0 +1,157 @@
+package com.neuedu.pojo;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Rooms extends MyPage implements Serializable {
+    private Integer id;
+
+    private String roomType;
+
+    private Integer roomTypeid;
+
+    private Integer status;
+
+    private String floorName;
+
+    private Integer floorId;
+
+    private BigDecimal normalPrice;
+
+    private Long discountPrice;
+
+    private BigDecimal gvipPrice;
+
+    private BigDecimal svipPrice;
+
+    private String remarks;
+
+    private Integer active;
+
+    private Date createDate;
+
+    private Floor floor;
+
+    private GuestType guestType;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType == null ? null : roomType.trim();
+    }
+
+    public Integer getRoomTypeid() {
+        return roomTypeid;
+    }
+
+    public void setRoomTypeid(Integer roomTypeid) {
+        this.roomTypeid = roomTypeid;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getFloorName() {
+        return floorName;
+    }
+
+    public void setFloorName(String floorName) {
+        this.floorName = floorName == null ? null : floorName.trim();
+    }
+
+    public Integer getFloorId() {
+        return floorId;
+    }
+
+    public void setFloorId(Integer floorId) {
+        this.floorId = floorId;
+    }
+
+    public BigDecimal getNormalPrice() {
+        return normalPrice;
+    }
+
+    public void setNormalPrice(BigDecimal normalPrice) {
+        this.normalPrice = normalPrice;
+    }
+
+    public Long getDiscountPrice() {
+        return discountPrice;
+    }
+
+    public void setDiscountPrice(Long discountPrice) {
+        this.discountPrice = discountPrice;
+    }
+
+    public BigDecimal getGvipPrice() {
+        return gvipPrice;
+    }
+
+    public void setGvipPrice(BigDecimal gvipPrice) {
+        this.gvipPrice = gvipPrice;
+    }
+
+    public BigDecimal getSvipPrice() {
+        return svipPrice;
+    }
+
+    public void setSvipPrice(BigDecimal svipPrice) {
+        this.svipPrice = svipPrice;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks == null ? null : remarks.trim();
+    }
+
+    public Integer getActive() {
+        return active;
+    }
+
+    public void setActive(Integer active) {
+        this.active = active;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Floor getFloor() {
+        return floor;
+    }
+
+    public void setFloor(Floor floor) {
+        this.floor = floor;
+    }
+
+    public GuestType getGuestType() {
+        return guestType;
+    }
+
+    public void setGuestType(GuestType guestType) {
+        this.guestType = guestType;
+    }
+}
---
+++ src/main/java/com/neuedu/service/impl/RoomsServiceImpl.java
@@ -0,0 +1,66 @@
+package com.neuedu.service.impl;
+
+import com.github.pagehelper.PageHelper;
+import com.neuedu.dao.RoomsMapper;
+import com.neuedu.pojo.Rooms;
+import com.neuedu.pojo.RoomsExample;
+import com.neuedu.service.FloorService;
+import com.neuedu.service.RoomsService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class RoomsServiceImpl implements RoomsService {
+    @Resource
+    RoomsMapper roomsMapper;
+    @Override
+    public List<Rooms> getRooms(Rooms rooms) {
+
+        PageHelper.startPage(rooms.getPageNo(), rooms.getPageSize());
+        RoomsExample roomsExample = new RoomsExample();
+        RoomsExample.Criteria criteria = roomsExample.createCriteria();
+        if (rooms.getId()!=null) {
+            criteria.andIdEqualTo(rooms.getId()).andActiveEqualTo(1);
+        }if(rooms.getFloorId()!=null){
+            criteria.andFloorIdEqualTo(rooms.getFloorId()).andActiveEqualTo(1);
+        }if(rooms.getRoomTypeid()!=null){
+            criteria.andRoomTypeidEqualTo(rooms.getRoomTypeid()).andActiveEqualTo(1);
+        }
+        else {
+            criteria.andActiveEqualTo(1);
+        }
+
+        return roomsMapper.selectByExample(roomsExample);
+    }
+
+    @Override
+    public List<Rooms> getAll(Rooms rooms) {
+        RoomsExample roomsExample = new RoomsExample();
+        return roomsMapper.selectByExample(roomsExample);
+    }
+
+    @Override
+    public Rooms getroomsById(int id) {
+        return roomsMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public int add(Rooms rooms) {
+        rooms.setCreateDate(new Date());
+        return roomsMapper.insertSelective(rooms);
+    }
+
+    @Override
+    public int del(Rooms rooms) {
+        return roomsMapper.updateByPrimaryKeySelective(rooms);
+    }
+
+    @Override
+    public int update(Rooms rooms) {
+        return roomsMapper.updateByPrimaryKeySelective(rooms);
+    }
+}
