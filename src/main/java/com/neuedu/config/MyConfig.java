5244f5c437bf6b16d1c5ebc0acfe2be023f8f291
zyp <759148450@qq.com>
2019-06-16 08:03:01 +0800

    细节修正

--- src/main/java/com/neuedu/config/MyConfig.java
+++ src/main/java/com/neuedu/config/MyConfig.java
@@ -41,7 +41,7 @@ public class MyConfig {
         return new Converter<String, Date>() {
             @Override
             public Date convert(String s) {
-                SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm");
+                SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
                 Date date=null;
                 try {
                     date=sdf.parse(s);
