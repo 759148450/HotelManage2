ef8a7cfa476177a01ce428027a13b5b0f2b1d2cd
l-vanilla <1135063038@qq.com>
2019-07-03 10:14:57 +0800

    用户的登录退出日志

--- src/main/java/com/neuedu/controller/UserController.java
+++ src/main/java/com/neuedu/controller/UserController.java
@@ -8,9 +8,12 @@ import com.neuedu.utils.MyLog;
 import org.apache.ibatis.annotations.Param;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.annotation.Resource;
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.util.List;
@@ -20,45 +23,64 @@ import java.util.List;
 public class UserController {
     @Resource
     UserService userService;
+
     @GetMapping("/list")
-    public PageInfo<User> getFloor(User user){
-        List<User> users =userService.getUsers(user);
+    public PageInfo<User> getFloor(User user) {
+        List<User> users = userService.getUsers(user);
         PageInfo<User> pageInfo = new PageInfo<>(users);
         return pageInfo;
     }
+
     @GetMapping("/getAll")
-    public List<User> getAll(User user){
+    public List<User> getAll(User user) {
         return userService.getAll(user);
     }
+
     @PostMapping("/add")
-    public int add(User user){
+    public int add(User user) {
         return userService.add(user);
     }
+
     @GetMapping("/del")
-    public int del(User user){
+    public int del(User user) {
         return userService.update(user);
     }
 
     @PostMapping("/update")
-    public int update(User user){
+    public int update(User user) {
         return userService.update(user);
     }
 
     @MyLog(value = "登录")  //这里添加了AOP的自定义注解
-    @CrossOrigin(allowCredentials="true")
+    @CrossOrigin(allowCredentials = "true")
     @PostMapping("/login")
-    public User login(@Param("userName") String userName, @Param("userPwd") String userPwd, HttpServletResponse response){
-        User user=userService.getUserByNameAndUserPwd(userName,userPwd);
+    public User login(@Param("userName") String userName, @Param("userPwd") String userPwd, HttpServletResponse response) {
+        User user = userService.getUserByNameAndUserPwd(userName, userPwd);
 //        session.setAttribute("user",user);
 //        System.out.println("session"+user.getUserName());
 //        UsernamePasswordToken token = new UsernamePasswordToken(userName, userPwd);
-        Cookie cookie=new Cookie("userName", user.getUserName());
-        cookie.setMaxAge(24*60*60); //存活期为1天
+        Cookie cookie = new Cookie("userName", user.getUserName());
+        cookie.setMaxAge(24 * 60 * 60); //存活期为1天
         cookie.setPath("/");
         response.addCookie(cookie);
-        System.out.println("cookie"+cookie.getValue());
+        System.out.println("cookie" + cookie.getValue());
         return user;
     }
+    @MyLog(value = "退出")  //这里添加了AOP的自定义注解
+    @GetMapping("/logout")
+    public Integer logout(HttpServletRequest request,
+                          HttpServletResponse response) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (int i = 0; i < cookies.length; i++) {
+                Cookie cookie = cookies[i];
+                cookie.setMaxAge(0);//销毁cookie
+                response.addCookie(cookie);
+            }
+        }
+        return 1;
+    }
+
     @GetMapping("/getOne")
     public User getOne(Integer id){
         return userService.getuserById(id);
